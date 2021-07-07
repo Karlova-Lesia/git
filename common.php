f281b1930c06034819952c682a2b5f83640b9bbe
Karlova Lesia <karlovalesa06@gmail.com>
2021-07-07 18:57:58 +0300

    Add lastname

---
+++ common.php
@@ -0,0 +1,12 @@
+<?php
+
+$people = [
+  [ "name" => "Pavel Butenko", "Role": "Student" ],
+  [ "name" => "Bogdan Onischenko", "Role": "Speaker" ],
+  [ "name" => "Artem Kuptsov", "Role": "Student" ],
+  [ "name" => "Alexey Goncharov", "Role": "Student" ],
+  [ "name" => "Vladyslav Cherniuk", "Role": "Student" ],
+  [ "name" => "Oleg Savenok", "Role": "Student" ],
+  [ "name" => "Yehor Hrytsenko",   "Role": "Student"],
+  [ "name" => "Karlova Lesia", "Role": "Student" ],
+];
